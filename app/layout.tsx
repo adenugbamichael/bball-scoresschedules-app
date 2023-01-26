7499de80eee7064a0ba02f9f16eec6b1a900eb8a
adenugbamichael <adenugbamicke@gmail.com>
2023-01-26 11:17:46 +0100

    Initialize commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import "./globals.css"
 
 export default function RootLayout({
   children,
@@ -6,12 +6,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html lang='en' className='antialiased'>
       <body>{children}</body>
     </html>
   )
--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    // Example: https://a.espncdn.com/i/teamlogos/ncaa/500/66.png
+    // Default: https://a.espncdn.com/i/teamlogos/default-team-logo-500.png
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "a.espncdn.com",
+        port: "",
+        pathname: "/i/teamlogos/ncaa/**",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
   experimental: {
     appDir: true,
   },
